436e9cd7ed10f9d5a6984b0a3b0c62d9074f8677
Vikash Yadav <yadav621gzp@gmail.com>
2024-10-01 21:07:54 +0530

    Update hardhat.config.js
    Increased Gas price

--- Frontend/src/SmartContract/hardhat.config.js
+++ Frontend/src/SmartContract/hardhat.config.js
@@ -16,6 +16,7 @@ module.exports = {
   networks:{
     hardhat:{},
     NeoTestnet: {
+      gasPrice: 40000000000,
       url: "https://neoxt4seed1.ngd.network",
       chainId: 12227332,
       accounts: ['5753e65f56865a161fbf41932a0d855139a4ce9dc20d82fb655bff393fc41702'],
